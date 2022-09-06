2bd1bc1016f7946c7ca6f44c540769676c33240f
Mikko De Los Reyes <mikkode212@gmail.com>
2022-09-06 11:43:06 -0400

    mapped through home, work, skills, contact, etc to create links in navbar

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -1,15 +1,23 @@
-import React from 'react';
-import './Navbar.scss';
-import { images } from '../../constants';
+import React from "react";
+import "./Navbar.scss";
+import { images } from "../../constants";
 
 const Navbar = () => {
   return (
-    <nav>
+    <nav className="app__navbar">
       <div>
         <img src={images.logo} alt="logo" />
       </div>
+      <ul>
+        {["home", "about", "contact", "work", "skills"].map((item) => (
+          <li key={`link-${item}`}>
+            <div/>
+            <a href={`#${item}`}>{item}</a>
+          </li>
+        ))}
+      </ul>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
