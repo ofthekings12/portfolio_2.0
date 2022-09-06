1e7fe1b934845542aa5f0d0014ad247993b4312a
Mikko De Los Reyes <mikkode212@gmail.com>
2022-09-06 14:15:04 -0400

    added a profile picture placeholder

--- frontend_react/src/container/Header/Header.jsx
+++ frontend_react/src/container/Header/Header.jsx
@@ -1,34 +1,43 @@
-import React from 'react';
-import './Header.scss';
-import { motion } from 'framer-motion';
-import { images } from '../../constants';
+import React from "react";
+import "./Header.scss";
+import { motion } from "framer-motion";
+import { images } from "../../constants";
 
 const Header = () => {
   return (
     <div className="app__header app__flex">
       <motion.div
-      whileInView={{ x: [-100, 0], opacity: [0, 1] }}
-      transition={{ duration: 0.5 }}
-      className="app__header-info">
+        whileInView={{ x: [-100, 0], opacity: [0, 1] }}
+        transition={{ duration: 0.5 }}
+        className="app__header-info"
+      >
         <div className="app__header-batch">
-          <div className="badge-cmp app__flex"
-          >
-            <span>
-              👋
-            </span>
+          <div className="badge-cmp app__flex">
+            <span>👋</span>
             <div style={{ marginLeft: 20 }}>
-              <p className='p-text'>Hello! I am</p>
-              <h1 className='head-text'>Mikko</h1>
+              <p className="p-text">Hello! I am</p>
+              <h1 className="head-text">Mikko</h1>
             </div>
           </div>
 
- 
+          <div className="tag-cmp app__flex">
+            <p className="p-text">Web Developer</p>
+            <p className="p-text">Artist</p>
+          </div>
         </div>
-
       </motion.div>
 
-    </div>
-  )
-}
+      <motion.div
+        whileInView={{ opacity: [0, 1] }}
+        transition={{ duration: 0.5, delayChildren: 0.5 }}
+        className="app__header-img"
+      >
+        <img src={images.profile} alt="profile_bg" />
 
-export default Header
+      </motion.div>
+      <motion.div></motion.div>
+    </div>
+  );
+};
+
+export default Header;
