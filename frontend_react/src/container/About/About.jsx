ec76074c3415d6d46432ffd88b624ab85e1796a9
Mikko De Los Reyes <mikkode212@gmail.com>
2022-09-08 10:08:49 -0400

    added placeholder about sections with title and description. also has motion animation

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -2,6 +2,12 @@ import React, { useState, useEffect } from 'react';
 import './About.scss';
 import { motion } from 'framer-motion';
 
+const abouts = [
+  { title: "Web Development", description: "I am a good developer", imgUrl: ''},
+  { title: "web Design", description: "I am a good developer", imgUrl: ''},
+  { title: "UI/UX", description: "I am a good developer", imgUrl: ''},
+  { title: "Full Stack", description: "I am a good developer", imgUrl: ''},
+]
 
 const About = () => {
   return (
@@ -9,9 +15,26 @@ const About = () => {
     <h2 className='head-text'>
       I Know That
       <span>Good Design</span>
+      <br />
       means
       <span>Good Business</span>
     </h2>
+
+    <div className="app__profile">
+    {abouts.map((about, index) => (
+      <motion.div
+      whileInView={{ opacity: 1 }}
+      whileHover={{ scale: 1.1 }}
+      transition={{ duration: 0.5, type: 'tween' }}
+      className="app__profile-item"
+      key={about.title + index}
+      >
+        <img src={about.imgUrl} alt={about.title} />
+        <h2 className='bold-text' style={{marginTop: 20 }}>{about.title}</h2>
+        <p className='p-text' style={{marginTop: 10 }}>{about.description}</p>
+      </motion.div>
+    ))}
+    </div>
     </>
   )
 }
