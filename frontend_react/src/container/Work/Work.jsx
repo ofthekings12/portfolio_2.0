0c824d8921b0f7abaa9bcd71f6323d6a23fab288
Mikko De Los Reyes <mikkode212@gmail.com>
2022-09-09 11:51:48 -0400

    created creative work section that maps through hard coded works

--- frontend_react/src/container/Work/Work.jsx
+++ frontend_react/src/container/Work/Work.jsx
@@ -1,10 +1,33 @@
-import React from 'react'
-import './Work.scss';
+import React, { useState, useEffect } from "react";
+import "./Work.scss";
+import { AiFillEye, AiFillGithub } from "react-icons/ai";
+import { motion } from "framer-motion";
+import { AppWrap } from "../../wrapper";
+import { urlFor, client } from "../../client";
 
 const Work = () => {
-  return (
-    <div>Work</div>
-  )
-}
 
-export default Work
+  const [activeFilter, setActiveFilter] = useState('All')
+
+  const handleWorkFilter = (item) => {
+
+  }
+
+  return (
+    <>
+      <h2 className="head-text">
+        My Creative <span>Portoflio</span>
+        <br /> Section
+      </h2>
+      <div className="app__work-filter">
+        {["Ui/Ux", "Web App", "Mobile App", "React App"].map((item, index) => (
+          <div key={index} 
+          onClick={() => handleWorkFilter(item)} 
+          className={`app__work-filter-item app__flex p-text ${activeFilter === item ? 'item-active' : '' }`}>{item}</div>
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default Work;
