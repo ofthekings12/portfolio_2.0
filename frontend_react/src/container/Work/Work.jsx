723e74c3f55b87b1239781cab321e2759f7055d9
Mikko De Los Reyes <mikkode212@gmail.com>
2022-09-09 12:34:23 -0400

    work projects now publishable via sanity CMS

--- frontend_react/src/container/Work/Work.jsx
+++ frontend_react/src/container/Work/Work.jsx
@@ -7,7 +7,21 @@ import { urlFor, client } from "../../client";
 
 const Work = () => {
 
-  const [activeFilter, setActiveFilter] = useState('All')
+  const [activeFilter, setActiveFilter] = useState('All');
+  const [animateCard, setAnimateCard] = useState({ y: 0, opacity: 1 });
+  const [works, setWorks] = useState([]);
+  const [filterWork, setFilterWork] = useState([]);
+
+  useEffect(() => {
+const query = '*[_type == "works"]';
+
+client.fetch(query)
+.then((data) => {
+  setWorks(data);
+  setFilterWork(data);
+})
+  }, [])
+  
 
   const handleWorkFilter = (item) => {
 
@@ -26,6 +40,21 @@ const Work = () => {
           className={`app__work-filter-item app__flex p-text ${activeFilter === item ? 'item-active' : '' }`}>{item}</div>
         ))}
       </div>
+
+      <motion.div
+      animate={animateCard}
+      transition={{ duration: 0.5, delayChildren: 0.5}}
+      className="app__work-portfolio">
+{filterWork.map((work, index) => (
+  <div className="app__work-item app__flex" key={index}>
+    <div className='app__work-img app__flex'>
+      <img src={urlFor(work.imgUrl)} alt={work.name}/>
+
+    </div>
+
+  </div>
+))}
+      </motion.div>
     </>
   );
 };
