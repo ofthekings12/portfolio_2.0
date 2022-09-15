bb86f58202a7ecd4fa456780e57f275ada614ef4
Mikko De Los Reyes <mikkode212@gmail.com>
2022-09-15 16:43:34 -0400

    replaced header logo and header tech icon circles

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -10,10 +10,10 @@ const Navbar = () => {
   return (
     <nav className="app__navbar">
       <div className="app__navbar-logo">
-        <img src={images.logo} alt="logo" />
+        <img src={images.HeaderLogo} alt="logo" />
       </div>
       <ul className="app__navbar-links">
-        {["home", "about", "contact", "work", "skills"].map((item) => (
+        {["home", "about", "work", "skills", "contact"].map((item) => (
           <li className="app__flex p-text" key={`link-${item}`}>
             <div />
             <a href={`#${item}`}>{item}</a>
--- frontend_react/src/components/NavigationDots.jsx
+++ frontend_react/src/components/NavigationDots.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const NavigationDots = ({ active }) => {
   return (
     <div className="app__navigation">
-      {["home", "about", "contact", "work", "skills", "testimonials"].map((item, index) => (
+      {["home", "about", "work", "skills", "contact", "testimonials"].map((item, index) => (
           <a href={`#${item}`} 
           key={item + index}
           className='app__navigation-dot'
